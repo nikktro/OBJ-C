8330ef5e4f4efc1166d00a92baa6625c9f15a8ad
Nikolay Trofimov <nikktro@gmail.com>
2020-10-09 10:04:32 +0300

    homework lesson1

--- objc_less01/main.m
+++ objc_less01/main.m
@@ -9,8 +9,38 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int first;
+        int second;
+        
+        printf("Enter first number: ");
+        scanf("%d", &first);
+        
+        printf("Enter second number: ");
+        scanf("%d", &second);
+        
+        
+        // Task 1
+        printf("--------Task1--------\n");
+        printf("The sum is %d\n", first + second);
+        printf("The substract is %d\n", first - second);
+        printf("The multiple is %d\n", first * second);
+        printf("The division is %d, with remainder %d\n", first / second, first % second);
+        
+        
+        // Task 2
+        printf("--------Task2--------\n");
+        NSLog(@"The sum is %i, The substract is %i, The multiple is %i, The division is %i, remainder is %i", first + second, first - second, first * second, first / second, first % second);
+        
+        // Task 3
+        printf("--------Task3--------\n");
+        int random1 = arc4random_uniform(10);
+        int random2 = arc4random_uniform(10);
+        int random3 = arc4random_uniform(10);
+        
+        printf("Three random values %d, %d, %d\n", random1, random2, random3);
+        printf("The AVG is %f\n", ((float)random1 + (float)random2 + (float)random3) / 3);
+        
     }
     return 0;
 }
